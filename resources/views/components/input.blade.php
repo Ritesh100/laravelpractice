25be9531d5a91e11e46d4123f9d1cb52ffccdba0
Ritesh100 <reteshthapakaji@gmail.com>
2022-11-01 20:20:06 +0545

    add component

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,9 @@
+<div>
+  
+<div class="form-group">
+              <label for="">{{$label}}</label>
+              <input type="{{$type}}" name="{{$name}}" id="" class="form-control" value="{{old('name')}}" placeholder="" aria-describedby="helpId">
+           
+              <!-- <small id="helpId" class="text-muted">Help text</small> -->
+            </div>
+</div>
